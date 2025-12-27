581d7c54e52f6cb349111cd19523fe29ab05b633
miaizhe <ftyfty2021@163.com>
2025-12-27 22:26:35 +0800

    更改赞助

--- src/config/sponsorConfig.ts
+++ src/config/sponsorConfig.ts
@@ -15,7 +15,7 @@ export const sponsorConfig: SponsorConfig = {
 		{
 			name: "支付宝",
 			icon: "fa6-brands:alipay",
-			qrCode: "/assets/images/sponsor/alipay.png", // 收款码图片路径（需要放在 public 目录下）
+			qrCode: "/src/content/posts/images/pay.jpg", // 收款码图片路径
 			link: "",
 			description: "使用 支付宝 扫码赞助",
 			enabled: true,
@@ -23,17 +23,9 @@ export const sponsorConfig: SponsorConfig = {
 		{
 			name: "微信",
 			icon: "fa6-brands:weixin",
-			qrCode: "/assets/images/sponsor/wechat.png", // 收款码图片路径
+			qrCode: "", // 图片不见了
 			link: "",
 			description: "使用 微信 扫码赞助",
-			enabled: true,
-		},
-		{
-			name: "爱发电",
-			icon: "simple-icons:afdian",
-			qrCode: "",
-			link: "https://afdian.com/a/cuteleaf",
-			description: "通过 爱发电 进行赞助",
 			enabled: true,
 		},
 		{
@@ -47,19 +39,5 @@ export const sponsorConfig: SponsorConfig = {
 	],
 
 	// 赞助者列表（可选）
-	sponsors: [
-		// 示例：已实名赞助者
-		{
-			name: "夏叶",
-			amount: "¥50",
-			date: "2025-10-01",
-			message: "感谢分享！",
-		},
-		// 示例：匿名赞助者
-		{
-			name: "匿名用户",
-			amount: "¥20",
-			date: "2025-10-01",
-		},
-	],
+	sponsors: [],
 };
